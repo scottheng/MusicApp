e1bf973a6fd73095d95c79caacff0e0a5e9760ac
Scott Heng <scottheng3@gmail.com>
2017-02-16 10:34:22 -0800

    Create helper methods to deal with session tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ActiveRecord::Base
   attr_reader :password
 
+  after_initialize :ensure_session_token
   validates :email, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: {message: "Password can't be empty"}
   validates :password, length: {minimum: 8, allow_nil: true}
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
 
   def password=(password)
     @password = password
@@ -12,6 +23,11 @@ class User < ActiveRecord::Base
 
   def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
   end
 
 
