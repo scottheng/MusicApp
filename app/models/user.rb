6751f5536414fb0457f7294406c047ab098de681
Scott Heng <scottheng3@gmail.com>
2017-02-16 10:20:45 -0800

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: {message: "Password can't be empty"}
+  # validates :password, length: {minimum: 8, allow_nil: true}
+
+
 end
