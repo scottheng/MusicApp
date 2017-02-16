b9ee1038eb5747cf68b527af6fac741eee1b00ec
Scott Heng <scottheng3@gmail.com>
2017-02-16 10:26:10 -0800

    Converts password to password_digest and checks password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
 class User < ActiveRecord::Base
+  attr_reader :password
+
   validates :email, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: {message: "Password can't be empty"}
-  # validates :password, length: {minimum: 8, allow_nil: true}
+  validates :password, length: {minimum: 8, allow_nil: true}
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 
 
 end
